0a49e87bdd9a1223177cb6fda670aa49670f4638
HFZR2005 <100490433+HFZR2005@users.noreply.github.com>
2024-06-29 00:11:06 +0100

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="bg-noise fixed top-0 left-0 w-full bg-pred p-6 z-10 border-b-4 border-black flex flex-row justify-between">
       <h1 className="text-4xl" style={{ fontFamily: 'Normandy Beach' }}>Sounds</h1>
       <div className="flex flex-row justify-between w-80">
-        <Button />
+        <Button Text={"About"}/>
         <DropDown />
       </div>
     </div>
